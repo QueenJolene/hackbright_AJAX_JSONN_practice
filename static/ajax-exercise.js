26a1a2e990f070984170a307f304e562ee6c08bb
Hackbright Student <info@hackbrightacademy.com>
2015-02-13 15:28:15 -0800

    Finished exercise 1

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -2,10 +2,10 @@
 
 function showFortune(evt) {
     // TODO: get the fortune and show it in the fortune-text div
+    $("#fortune-text").load("/fortune");
 }
 
 $('#get-fortune-button').on('click', showFortune);
-
 
 
 
@@ -16,6 +16,14 @@ function showWeather(evt) {
     evt.preventDefault();
     var url = "/weather?zipcode=" + $("#zipcode-field").val();
     // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(
+        "/weather",
+        $("#weather-form").serialize(),
+        function(result) {
+            var forcast_dict = JSON.parse(result);
+            $("#weather-info").load(forcast_dict[forcast]);
+            
+        });
 }
 
 $("#weather-form").on('submit', showWeather);
